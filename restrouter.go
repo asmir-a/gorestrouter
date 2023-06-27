56a3f2369fe9a8ed4302161579d357bbe2751fa6
Asmir <asmir8717@gmail.com>
2023-06-27 20:16:53 +0900

    implemented and tested the first version of the router

--- restrouter.go
+++ restrouter.go
@@ -1,1 +1,30 @@
 package restrouter
+
+import (
+	"net/http"
+	"path"
+
+	"github.com/asmir-a/gorestrouter/endpointparser"
+	"github.com/asmir-a/gorestrouter/executor"
+	"github.com/asmir-a/gorestrouter/resource"
+	"github.com/asmir-a/gorestrouter/tree"
+)
+
+type Router struct {
+	endpoints []resource.Url
+}
+
+func (router *Router) Handle(endpointPath string, handlerBuilder func(map[string]string) http.Handler) {
+	url := endpointparser.ParseToUrl(endpointPath, handlerBuilder)
+	router.endpoints = append(router.endpoints, url)
+}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	urlsTree := tree.NewUrlsTree(router.endpoints) //the abstraction boundaries are bit weird right now. First, router should contain an executor inside of it. Also, the router should be able to insert a path to executor after every handler is called. Router prolly should not have access to tree variable. The serveHttp prolly just should call FindHandlerAndHandle
+	requestExecutor := executor.NewExecutor(urlsTree)
+
+	endpointPath := req.URL.Path
+	endpointPath = path.Clean(endpointPath)
+
+	requestExecutor.FindHandlerAndHandle(w, req, endpointPath)
+}
--- restrouter_test.go
+++ restrouter_test.go
@@ -1,1 +1,41 @@
 package restrouter
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRestRouterGeneral(t *testing.T) {
+	router := &Router{}
+	handlerBuilder := func(params map[string]string) http.Handler {
+		username := params["username"]
+		handler := func(w http.ResponseWriter, req *http.Request) {
+			w.Write([]byte(username))
+		}
+		return http.HandlerFunc(handler)
+	}
+	router.Handle("/[username]/wordgame/stats", handlerBuilder)
+
+	server := httptest.NewServer(router)
+	baseServerUrl := server.URL
+	defer server.Close()
+
+	client := http.Client{}
+	response, err := client.Get(baseServerUrl + "/asmir/wordgame/stats")
+	if err != nil {
+		t.Fatal("request could not be sent to server: ", err)
+	}
+
+	defer response.Body.Close()
+
+	bodyBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal("could not read response body: ", err)
+	}
+	bodyString := string(bodyBytes)
+	if bodyString != "asmir" {
+		t.Fatal("the server should have sent asmir to the client, but sent", bodyString)
+	}
+}
